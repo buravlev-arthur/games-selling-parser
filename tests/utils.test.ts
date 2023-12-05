3759910ab92997a09bd05c650b8d53ed8e84fe84
buravlev-arthur <buravlev-arthur@yandex.ru>
2023-12-05 17:20:04 -0500

    Types in utils test was added

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -6,30 +6,26 @@ import { getRandomUserAgent, createAxiosInstance, parseBuffer } from '@/utils';
 describe('Test utils', () => {
   it('getRandomUserAgent', () => {
     const value = getRandomUserAgent();
-    expect(userAgents.includes(value)).toBeTruthy();
+    expect<boolean>(userAgents.includes(value)).toBeTruthy();
   });
 
   it('createAxiosInstance', () => {
     const baseUrl = 'https://test.test';
     const contentType = 'text/html';
     const axios = createAxiosInstance(baseUrl, contentType);
-    expect(axios.defaults.baseURL).toEqual(baseUrl);
-    expect(axios.defaults.responseType).toEqual('arraybuffer');
-    expect(axios.defaults.decompress).toEqual(false);
-    expect(userAgents.includes(axios.defaults.headers['User-Agent'] as string)).toBeTruthy();
-    expect(axios.defaults.headers['Content-Type']).toEqual(contentType);
-    expect(axios.defaults.headers['Accept-Encoding']).toEqual('gzip');
+    expect<string>(axios.defaults.baseURL).toEqual(baseUrl);
+    expect<string>(axios.defaults.responseType).toEqual('arraybuffer');
+    expect<boolean>(axios.defaults.decompress).toEqual(false);
+    expect<boolean>(userAgents.includes(axios.defaults.headers['User-Agent'] as string)).toBeTruthy();
+    expect<string>(axios.defaults.headers['Content-Type'] as string).toEqual(contentType);
+    expect<string>(axios.defaults.headers['Accept-Encoding'] as string).toEqual('gzip');
   });
 
   it('parseBuffer', async () => {
     const { withComporessing, withoutCompressing } = binaryResponse;
-    expect(await parseBuffer(withoutCompressing.response, 'json')).toEqual(
-      withoutCompressing.json as unknown as undefined,
-    );
-    expect(await parseBuffer(withoutCompressing.response, 'text')).toEqual(
-      withoutCompressing.text as unknown as undefined,
-    );
-    expect(await parseBuffer(withComporessing.response, 'json')).toEqual(withComporessing.json as unknown as undefined);
-    expect(await parseBuffer(withComporessing.response, 'text')).toEqual(withComporessing.text as unknown as undefined);
+    expect<object>(await parseBuffer(withoutCompressing.response, 'json')).toEqual(withoutCompressing.json);
+    expect<string>(await parseBuffer(withoutCompressing.response, 'text')).toEqual(withoutCompressing.text);
+    expect<object>(await parseBuffer(withComporessing.response, 'json')).toEqual(withComporessing.json);
+    expect<string>(await parseBuffer(withComporessing.response, 'text')).toEqual(withComporessing.text);
   });
 });
